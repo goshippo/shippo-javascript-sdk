f619e8c15192d6db16f8e23ad69c6d2422ba3783
Randall Keur <randall.keur@shippo.com>
2024-11-20 10:33:55 -0500

    update test to handle empty results

--- test/order.test.ts
+++ test/order.test.ts
@@ -1,5 +1,4 @@
 import {shippoSdk} from "./helpers";
-import {OrderStatusEnum} from "../src";
 import {expect} from "chai";
 
 
@@ -7,23 +6,29 @@ describe('TestOrders', function () {
     it('testListOrders', async () => {
         const orders = await shippoSdk.orders.list({
             page: 1,
-            results: 25,
-            orderStatus: [OrderStatusEnum.Paid]
+            results: 25
         })
 
         expect(orders).to.not.be.null;
-        expect(orders.results).to.be.an('array').that.is.not.empty;
+        expect(orders.results).to.be.an('array');
 
-        expect(orders.results).to.satisfy(results =>
-            results.every(order =>
-                order.orderStatus = [OrderStatusEnum.Paid] &&
-                order &&
-                typeof order.objectId === 'string' &&
-                typeof order.toAddress === 'object' &&
-                typeof order.fromAddress === 'object' &&
-                Array.isArray(order.transactions)
-            )
-        );
+        if (orders.results.length > 0) {
+            expect(orders.results).to.satisfy(results =>
+                results.every(order =>
+                    order &&
+                    typeof order.objectId === 'string' &&
+                    order.toAddress &&
+                    typeof order.toAddress.country === 'string' &&
+                    typeof order.toAddress.city === 'string' &&
+                    typeof order.toAddress.street1 === 'string' &&
+                    typeof order.fromAddress === 'object' &&
+                    typeof order.fromAddress.country === 'string' &&
+                    typeof order.fromAddress.city === 'string' &&
+                    typeof order.fromAddress.street1 === 'string' &&
+                    Array.isArray(order.transactions)
+                )
+            );
+        }
 
     });
 })
